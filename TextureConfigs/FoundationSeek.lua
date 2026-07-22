8229be25a64518c87756e29581ed24a97439d430
RANDOMZIRBLAAA <blablavuivui@gmail.com>
2026-07-22 10:38:16 +0700

    Create FoundationSeek.lua

---
+++ TextureConfigs/FoundationSeek.lua
@@ -0,0 +1,24 @@
+return {
+		Name = "Foundation Seek",
+		Image = "rbxassetid://124462971884449",
+		Light = {
+			Enabled = true,
+			Brightness = 1,
+			Range = 20,
+			Color = Color3.fromRGB(255,0,0)
+		}, 
+		
+		Sound = {
+		Enabled = true,
+		Url = "https://github.com/cakmay227-svg/random/raw/refs/heads/main/YTDown.com_YouTube_Terminal-Velocity_Media_jqc4w2Yg62U_001_1080p.mp3",
+		FileName = "JHDEJJDJJDJJDJ",
+		Extension = "mp3", 
+		Volume = 2
+	}, 
+	
+	Voiceline = {
+	Enabled = true,
+	Id = "rbxassetid://133358860191747",
+	Volume = 1
+}
+	},

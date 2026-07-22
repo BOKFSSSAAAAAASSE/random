5f1a34c86dc628295f8cdd966ec2fd9097943538
RANDOMZIRBLAAA <blablavuivui@gmail.com>
2026-07-22 10:34:45 +0700

    Create Loader.lua

---
+++ TextureConfigs/Loader.lua
@@ -0,0 +1,17 @@
+local Base = "https://raw.githubusercontent.com/USERNAME/random/main/TextureConfigs/"
+
+local Files = {
+	"Seek",
+	"Rush",
+	"Ambush",
+	"Figure"
+}
+
+local Textures = {}
+
+for _, Name in ipairs(Files) do
+	Textures[#Textures + 1] =
+		loadstring(game:HttpGet(Base .. Name .. ".lua"))()
+end
+
+return Textures

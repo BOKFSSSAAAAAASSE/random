92b5d600c78f415c29a5221d98c086b5e81aa4ed
RANDOMZIRBLAAA <blablavuivui@gmail.com>
2026-07-22 10:36:33 +0700

    Update Seek.lua

--- TextureConfigs/Seek.lua
+++ TextureConfigs/Seek.lua
@@ -1,1 +1,24 @@
-
+return {
+		Name = "Seek",
+		Image = "rbxassetid://108816498055385",
+		Light = {
+			Enabled = true,
+			Brightness = 1,
+			Range = 20,
+			Color = Color3.fromRGB(255,0,0)
+		}, 
+		
+		Sound = {
+		Enabled = true,
+		Url = "https://github.com/cakmay227-svg/random/raw/refs/heads/main/YTDown.com_YouTube_Roblox-Doors-OST-Here-I-Come-Seek-s-Chas_Media_LznNB8eUuYY_001_1080p.mp3",
+		FileName = "DWSEJENDND",
+		Extension = "mp3", 
+		Volume = 2
+	}, 
+	
+	Voiceline = {
+	Enabled = true,
+	Id = "rbxassetid://133358860191747",
+	Volume = 1
+}
+	},

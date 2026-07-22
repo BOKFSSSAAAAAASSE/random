9ed041eeaf3f1b056321e4268a6d5a26b9692baa
RANDOMZIRBLAAA <blablavuivui@gmail.com>
2026-07-22 10:47:04 +0700

    Update Loader.lua

--- TextureConfigs/Loader.lua
+++ TextureConfigs/Loader.lua
@@ -1,4 +1,4 @@
-local Base = "https://raw.githubusercontent.com/BOKFSSSAAAAAASSE/random/main/TextureConfigs/"
+local Base = "https://raw.githubusercontent.com/BOKFSSSAAAAAASSE/random/main/TextureConfigs/Loader.lua"
 
 local Files = {
 	"Seek",
